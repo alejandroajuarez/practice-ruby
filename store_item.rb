bce44c6adca10d0849ba0691e255301c0e6799fe
alejandrojuarez117 <aljuarez117@gmail.com>
2025-01-07 19:52:29 -0700

    create class and method that print from an array

--- store_item.rb
+++ store_item.rb
@@ -5,6 +5,10 @@
 # C.2 - Represent 3 items using hashes. Each hash should have the same keys with different values.
 # C.3 - Be sure to use symbols for the keys. Try creating hashes using both types of hash symbol syntaxes. (Ruby syntax {:a => 123} vs. “JavaScript” syntax {a: 123}).
 
+# a) Choose which attributes should have “reader” methods and which attributes should have “writer” methods.
+# b) Create an instance from your store item class. Use puts statements to print the 3 attributes individually to the terminal.
+# c) Use the attr_writer method to change a value
+
 # item1 = {:product => "Television", :price => 499, :brand => "LG"}
 # item2 = {:product => "Washer", :price => 899, :brand => "LG"}
 # item3 = {product: "Refrigerator", price: 1299, brand: "Samsung"}
@@ -12,22 +16,25 @@
 # puts item1, item2, item3
 
 class Store
-  def initialize(product, price, brand)
+  def initialize(product, price, brand, condition)
     @product = product
     @price = price
     @brand = brand
+    @condition = condition
     
-    puts "This store has sells #@product, for $#@price, #@brand!"
   end
 
-  def print_one(product, price, brand)
-    @product = product
-    @price = price
-    @brand = brand
-    puts item1
+  def print_store
+    puts "This store sells #@condition #@product, for $#@price, from #@brand!"
+  end
+
+  def read_write
+    
   end
 end
 
-item1 = Store.new("Television", 499, "LG")
-item2 = Store.new("Washer", 899, "LG")
-item3 = Store.new("Refrigerator", 1299, "Samsung")
+item1 = Store.new("Televisions", 499, "LG", "New")
+item2 = Store.new("Washers", 899, "LG", "Used")
+item3 = Store.new("Refrigerators", 1299, "Samsung", "New")
+
+puts item1.print_store, item2.print_store, item3.print_store
